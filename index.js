4d71ffa7763e54957044dff4cf757c594173814e
abcode <mabsattar@proton.me>
2024-09-09 16:30:23 +0530

    initialized JS

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
-const firstNameCheck = document.getElementById('fname')
-const lastNameCheck = document.getElementById('lname')
-const emailCheck = document.getElementById('mail')
+const firstNameCheck = document.getElementById('firstname')
+const lastNameCheck = document.getElementById('lastname')
+const emailCheck = document.getElementById('emailaddress')
+const queryCheck = document.querySelector('input[name = "queryType"]')
+const messageCheck = document.getElementById('msg')
+const checkbox = document.getElementById('input[name = "checkbox"]')
 
-if (!firstNameCheck || !lastNameCheck || !emailCheck) {
-    showError("Please fill out all the required fields")
-    return false;
-}
+const form = document.querySelector('.contact-form')
+
+const validation = () => {
+    const firstName = firstNameCheck.value.trim();
+    const lastName = lastNameCheck.value.trim();
+    const emailAddress = emailCheck.value.trim();
+    const message = messageCheck.value.trim();
+
+
+    if (!firstName || !lastName || !emailAddress || !message) {
+        ShowError("Please fill all the required fields");
+        return false
+    }
+
+    if (!isValidEmail(emailAddress)) {
+        ShowError("Please enter a valid email address.");
+        return false;
+    }
+
+    else showSuccessMessage("Form submitted successfully!");
+    return true;
+};
+
+
